21757ce50f0a65fdd8e2146f933a49124a75f30e
Shubhambth <mixmemebth@gmail.com.com>
2025-04-03 00:28:36 +0530

    v2

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -82,12 +82,26 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'GoZlKEJjlaUMUaljKMiDuzOIFaMfrxtz',
+        'HOST': 'yamanote.proxy.rlwy.net',
+        'PORT': '59804',
     }
 }
+
 
 
 # Password validation
